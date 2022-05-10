12ba49597e83bd5a7ec21fc641a7a437b81cd249
Jaman Uddin Khan <jamanuddin.khan@gmail.com>
2022-05-10 17:31:06 +0600

    Added date funtions

--- index.php
+++ index.php
@@ -5,8 +5,12 @@
 <h1>PHP - Codepipeline</h1>
 
 <?php
-echo "Hello World!";
-?> 
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "Today is " . date("Y.m.d") . "<br>";
+echo "Today is " . date("Y-m-d") . "<br>";
+echo "Today is " . date("l");
+?>
+
 
 </body>
 </html>
